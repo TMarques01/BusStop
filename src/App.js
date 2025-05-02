b323c2f55ab6d4daa4c87bfc8fce9079b29d9696
RuiOliv4444 <132606623+RuiOliv4444@users.noreply.github.com>
2025-05-02 11:57:54 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import MapPage from './components/MapPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MapPage from './components/MapPage.js';
+import Historic from './components/Historic.js';
+import Search from'./components/Search.js';
+import Menu from './components/Menu.js';
 import './App.css';
-import Menu from './components/Menu'; 
-import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-    <div>
-		<Menu />
-      <MapPage />
-    </div>
+    <Router>
+      <div>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<MapPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/history" element={<Historic />} />
+		      <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
